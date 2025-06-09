120ce5f03f8aa5305f18d719327a168eeadf26fe
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-09 18:33:18 -0300

    feat: criando a rota de criacao de uma transacao

---
+++ src/routes/transactions.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from "node:crypto";
+import { FastifyInstance } from "fastify";
+import { z } from 'zod';
+
+import { knex } from "../database";
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, repy) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string().min(3),
+      amount: z.coerce.number().min(1),
+      type: z.enum(['credit', 'debit'])
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return repy.status(201).send();
+  })
+
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*');
+    return transactions
+  })
+}
