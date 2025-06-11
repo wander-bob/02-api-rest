44399d187f3c7510cf30298d85760b61039a71e1
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-11 00:30:40 -0300

    feat: adicionando os testes de listagem de todas as transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,9 +1,9 @@
-import { describe, it, beforeAll, afterAll } from 'vitest';
+import { describe, it, beforeAll, afterAll, expect } from 'vitest';
 import supertest from 'supertest';
 
 import { app } from '../src/app';
 
-describe('Transactions route', () => {
+describe('Transactions routes', () => {
 	beforeAll(async () => {
 		await app.ready();
 	});
@@ -20,4 +20,27 @@ describe('Transactions route', () => {
 				type: 'credit'
 			}).expect(201);
 	});
+
+	it('should be able to list all transactions', async () => {
+		const createdTransactions = await supertest(app.server).post('/transactions')
+			.send({
+				title: 'New test transactions',
+				amount: 1000,
+				type: 'credit'
+			});
+
+		const cookies = createdTransactions.get('Set-Cookie');
+
+		const listOfTransactions = await supertest(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies!)
+			.expect(200);
+
+		expect(listOfTransactions.body.transactions).toEqual([
+			expect.objectContaining({
+				title: 'New test transactions',
+				amount: 1000,
+			})
+		])
+	})
 })
