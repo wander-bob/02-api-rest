dd4a2e9f4577eb2b3943732868c0563e86b88dc3
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-06-12 00:39:31 -0300

    feat: Finalizando a configuração do ambiente para o deploy

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,13 @@
 import knexSetup, { Knex } from 'knex';
 import { env } from './env/'
 
+const DATABASE_CONNECTION = env.DATABASE_CLIENT === "sqlite" ? {
+  filename: env.DATABASE_URL
+} : env.DATABASE_URL;
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: env.DATABASE_CLIENT,
+  connection: DATABASE_CONNECTION,
   migrations: {
     extension: 'ts',
     directory: './db/migrations'
--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333)
+  PORT: z.coerce.number().default(3333)
 })
 
 export const _env = envSchema.safeParse(process.env);
